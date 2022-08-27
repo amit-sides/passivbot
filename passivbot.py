d52ef3b99ca0b51369c96b2aeeb65bb96f944099
Amit Sides <amit@aramnik.com>
2022-08-27 23:25:16 +0300

    Removed log

--- passivbot.py
+++ passivbot.py
@@ -346,7 +346,6 @@ class Bot:
                 try:
                     o = await c
                     created_orders.append(o)
-                    logging.info(f"O: {o}")
                     if "side" in o:
                         logging.info(
                             f'  created order {o["symbol"]} {o["side"]: <4} '
